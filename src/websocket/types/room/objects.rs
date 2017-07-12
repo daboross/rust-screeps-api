021f8ce541a219a3f8eaaacdb10ee783402a8015
David Ross <daboross@daboross.net>
2017-07-11 19:08:45 -0700

    Keeper lairs

--- src/websocket/types/room/objects.rs
+++ src/websocket/types/room/objects.rs
@@ -302,8 +302,24 @@ with_structure_fields_and_update_struct! {
     }
 }
 
+with_base_fields_and_update_struct! {
+    /// A keeper lair - a structure which spawns npc hostiles to protect minerals and resources nearby.
+    #[derive(Clone, Debug, PartialEq)]
+    #[serde(rename_all = "camelCase")]
+    pub struct StructureKeeperLair {
+        /// The next game time at which this structure will spawn an NPC.
+        ///
+        /// If an NPC this structure spawned is currently alive, the structure waits until that NPC dies
+        /// before starting the timer. This is what a null/None value indicates.
+        pub next_spawn_time: Option<u32>,
+    }
 
-//
+    /// The update structure for a keeper lair structure.
+    #[derive(Clone, Debug, PartialEq)]
+    #[serde(rename_all = "camelCase")]
+    pub struct StructureKeeperLairUpdate { ... }
+}
+
 
 // #[derive(Clone, Debug, Hash)]
 // pub enum RoomObject {
@@ -330,7 +346,7 @@ mod test {
     use data::{RoomName, RoomSign};
 
     use super::{Source, Controller, ControllerReservation, Mineral, StructureSpawn, SpawningCreep, StructureExtension,
-                StructureRoad, StructureWall, StructureRampart};
+                StructureRoad, StructureWall, StructureRampart, StructureKeeperLair};
 
     #[test]
     fn parse_source_and_update() {
@@ -861,6 +877,82 @@ mod test {
             next_decay_time: 19894001,
             notify_when_attacked: true,
             user: "57874d42d0ae911e3bd15bbc".to_owned(),
+        });
+    }
+
+    #[test]
+    fn parse_keeper_lair_spawned_and_update() {
+        let json = json!({
+            "_id": "55c34a6b5be41a0a6e80c325",
+            // Guessing this is a legacy value that's in the database from when this was the game tick time.
+            //
+            // This only appears on objects in rooms near the center of the map (the oldest part).
+            "_updated": 13052933,
+            "nextSpawnTime": null,
+            "room": "E4N4",
+            "type": "keeperLair",
+            "x": 16,
+            "y": 1
+        });
+
+        let mut obj = StructureKeeperLair::deserialize(json).unwrap();
+
+        assert_eq!(obj, StructureKeeperLair {
+            room: RoomName::new("E4N4").unwrap(),
+            x: 16,
+            y: 1,
+            id: "55c34a6b5be41a0a6e80c325".to_owned(),
+            next_spawn_time: None,
+        });
+
+        obj.update(serde_json::from_value(json!({
+            "nextSpawnTime": 20000000,
+        }))
+            .unwrap());
+
+        assert_eq!(obj, StructureKeeperLair {
+            room: RoomName::new("E4N4").unwrap(),
+            x: 16,
+            y: 1,
+            id: "55c34a6b5be41a0a6e80c325".to_owned(),
+            next_spawn_time: Some(20000000),
+        });
+
+    }
+
+    #[test]
+    fn parse_keeper_lair_spawning_and_update() {
+        let json = json!({
+            "_id": "55c34a6b5be41a0a6e80c6ce",
+            "_updated": 13052933,
+            "nextSpawnTime": 20184048,
+            "room": "W14S5",
+            "type": "keeperLair",
+            "x": 22,
+            "y": 13
+        });
+
+        let mut obj = StructureKeeperLair::deserialize(json).unwrap();
+
+        assert_eq!(obj, StructureKeeperLair {
+            room: RoomName::new("W14S5").unwrap(),
+            x: 22,
+            y: 13,
+            id: "55c34a6b5be41a0a6e80c6ce".to_owned(),
+            next_spawn_time: Some(20184048),
+        });
+
+        obj.update(serde_json::from_value(json!({
+            "nextSpawnTime": null,
+        }))
+            .unwrap());
+
+        assert_eq!(obj, StructureKeeperLair {
+            room: RoomName::new("W14S5").unwrap(),
+            x: 22,
+            y: 13,
+            id: "55c34a6b5be41a0a6e80c6ce".to_owned(),
+            next_spawn_time: None,
         });
     }
 }
