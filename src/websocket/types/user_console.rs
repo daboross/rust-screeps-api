dae5503e4b7140a29e6b0378ae0042d095330bf2
David Ross <daboross@daboross.net>
2017-08-13 00:39:59 -0700

    Fix console websocket parsing for shards

--- src/websocket/types/user_console.rs
+++ src/websocket/types/user_console.rs
@@ -1,5 +1,5 @@
 //! Update parsing for console message updates.
-use serde::de::{Deserialize, Deserializer};
+use serde::de::{Deserialize, Deserializer, Error};
 
 /// Update for a user's log messages during the last tick.
 #[derive(Clone, Hash, Debug)]
@@ -11,29 +11,42 @@ pub enum UserConsoleUpdate {
         log_messages: Vec<String>,
         /// All result strings from console commands executed last tick.
         result_messages: Vec<String>,
+        /// The shard the update is from
+        shard: Option<String>,
     },
     /// An error occurred in the user script. May be sent multiple times per tick,
     /// and will not be sent unless an error did occur.
     Error {
         /// The error which occurred.
         message: String,
+        /// The shard the update is from
+        shard: Option<String>,
     },
+}
+
+impl UserConsoleUpdate {
+    pub fn shard(&self) -> Option<&str> {
+        match *self {
+            UserConsoleUpdate::Messages { ref shard, .. } => shard.as_ref().map(|s| s.as_ref()),
+            UserConsoleUpdate::Error { ref shard, .. } => shard.as_ref().map(|s| s.as_ref()),
+        }
+    }
 }
 
 // Separate representation for deserializing needed in order to have 'Error' variant be a
 // struct-like variant rather than a tuple-like variant.
 
 #[derive(Deserialize, Debug)]
-enum InnerUpdateRepresentation {
-    #[serde(rename = "messages")]
-    Messages {
-        #[serde(rename = "log")]
-        log_messages: Vec<String>,
-        #[serde(rename = "results")]
-        result_messages: Vec<String>,
-    },
-    #[serde(rename = "error")]
-    Error(String),
+struct InnerUpdateInnerMessages {
+    log: Vec<String>,
+    results: Vec<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct InnerUpdateRepresentation {
+    error: Option<String>,
+    messages: Option<InnerUpdateInnerMessages>,
+    shard: Option<String>,
 }
 
 impl<'de> Deserialize<'de> for UserConsoleUpdate {
@@ -43,15 +56,30 @@ impl<'de> Deserialize<'de> for UserConsoleUpdate {
     {
         let intermediate = InnerUpdateRepresentation::deserialize(deserializer)?;
 
-        Ok(match intermediate {
-            InnerUpdateRepresentation::Messages {
-                log_messages,
-                result_messages,
-            } => UserConsoleUpdate::Messages {
-                log_messages: log_messages,
-                result_messages: result_messages,
+        let InnerUpdateRepresentation {
+            error: error_opt,
+            messages: messages_opt,
+            shard,
+        } = intermediate;
+
+        let parsed = match (error_opt, messages_opt) {
+            (Some(e), None) => UserConsoleUpdate::Error {
+                message: e,
+                shard: shard,
             },
-            InnerUpdateRepresentation::Error(message) => UserConsoleUpdate::Error { message: message },
-        })
+            (None, Some(m)) => UserConsoleUpdate::Messages {
+                log_messages: m.log,
+                result_messages: m.results,
+                shard: shard,
+            },
+            (Some(_), Some(_)) => {
+                return Err(D::Error::custom("expected either 'messages' property or 'error' property, not both"));
+            }
+            (None, None) => {
+                return Err(D::Error::custom("expected either 'messages' property or 'error' property, neither found"));
+            }
+        };
+
+        Ok(parsed)
     }
 }
