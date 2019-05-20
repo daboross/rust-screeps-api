bc97e01770da5c77af9a754c607547846979ac1b
David Ross <daboross@daboross.net>
2019-05-20 02:07:20 -0700

    Fix support for ownerless extractors

--- src/websocket/types/room/objects/extractor.rs
+++ src/websocket/types/room/objects/extractor.rs
@@ -7,11 +7,12 @@ with_structure_fields_and_update_struct! {
     #[serde(rename_all = "camelCase")]
     pub struct StructureExtractor {
         /// The user ID of the owner of this structure.
-        pub user: String,
+        pub user: Option<String>,
         /// Whether or not this structure is non-functional due to a degraded controller.
         #[serde(default, rename = "off")]
         pub disabled: bool,
         /// Whether or not an attack on this structure will send an email to the owner automatically.
+        #[serde(default)]
         pub notify_when_attacked: bool,
     }
 
@@ -19,7 +20,7 @@ with_structure_fields_and_update_struct! {
     #[derive(Clone, Debug)]
     #[serde(rename_all = "camelCase")]
     pub struct StructureExtractorUpdate {
-        - user: String,
+        - user: Option<String>,
         #[serde(rename = "off")]
         - disabled: bool,
         - notify_when_attacked: bool,
@@ -61,7 +62,36 @@ mod test {
                 hits_max: 500,
                 notify_when_attacked: true,
                 disabled: false,
-                user: "5ca80c8f3c33e30c8e85555d".to_owned(),
+                user: Some("5ca80c8f3c33e30c8e85555d".to_owned()),
+            }
+        );
+    }
+
+    #[test]
+    fn parse_keeper_room_extractor() {
+        let json = json!({
+            "_id": "59834302d7922107c0781386",
+            "cooldown": 0,
+            "room": "W5S35",
+            "type": "extractor",
+            "x": 9,
+            "y": 36,
+        });
+
+        let obj = StructureExtractor::deserialize(json).unwrap();
+
+        assert_eq!(
+            obj,
+            StructureExtractor {
+                room: RoomName::new("W5S35").unwrap(),
+                x: 9,
+                y: 36,
+                id: "59834302d7922107c0781386".to_owned(),
+                hits: 0,
+                hits_max: 0,
+                notify_when_attacked: false,
+                disabled: false,
+                user: None,
             }
         );
     }
