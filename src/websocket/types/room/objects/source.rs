746cc56d2308a62f70b0dd56f88ec6e30abfb8b1
David Ross <daboross@daboross.net>
2019-05-21 20:47:52 -0700

    Handle null invader_harvested in Source

--- src/websocket/types/room/objects/source.rs
+++ src/websocket/types/room/objects/source.rs
@@ -12,6 +12,7 @@ with_base_fields_and_update_struct! {
         pub energy_capacity: u32,
         /// The amount of energy either harvested on this source specifically or for the room since
         /// the last invasion (not sure which it is).
+        #[serde(default)]
         pub invader_harvested: u32,
         /// The game time at which the source will next regenerate. `None` when waiting on a creep to
         /// first harvest it.
@@ -24,7 +25,13 @@ with_base_fields_and_update_struct! {
     /// The update structure for a source object.
     #[derive(Clone, Debug)]
     #[serde(rename_all = "camelCase")]
-    pub struct SourceUpdate { ... }
+    pub struct SourceUpdate {
+        - energy: u32,
+        - energy_capacity: u32,
+        - invader_harvested: u32,
+        - next_regeneration_time: Option<u64>,
+        - ticks_to_regeneration: u32,
+    }
 }
 
 #[cfg(test)]
@@ -88,6 +95,37 @@ mod test {
                 energy_capacity: 3000,
                 invader_harvested: 29240,
                 next_regeneration_time: Some(19894026),
+                ticks_to_regeneration: 300,
+            }
+        );
+    }
+
+    fn handle_no_invader_harvested() {
+        let json = json!({
+            "_id": "5bbcad499099fc012e6370ab",
+            "energy": 4000,
+            "energyCapacity": 4000,
+            "nextRegenerationTime": null,
+            "room": "E6S26",
+            "ticksToRegeneration": 300,
+            "type": "source",
+            "x": 40,
+            "y": 34,
+        });
+
+        let mut obj = Source::deserialize(&json).unwrap();
+
+        assert_eq!(
+            obj,
+            Source {
+                id: "5bbcad499099fc012e6370ab".to_owned(),
+                room: RoomName::new("E6S26").unwrap(),
+                x: 40,
+                y: 34,
+                energy: 4000,
+                energy_capacity: 4000,
+                invader_harvested: 0,
+                next_regeneration_time: None,
                 ticks_to_regeneration: 300,
             }
         );
