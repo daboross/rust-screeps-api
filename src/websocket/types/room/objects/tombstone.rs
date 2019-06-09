fdd08604358ead209fb9890df087c416f5a648f8
David Ross <daboross@daboross.net>
2019-06-08 19:54:57 -0700

    Add tombstone resource iterator

--- src/websocket/types/room/objects/tombstone.rs
+++ src/websocket/types/room/objects/tombstone.rs
@@ -131,6 +131,117 @@ with_resource_fields_and_update_struct! {
     pub struct TombstoneUpdate { ... }
 }
 
+resource_iterator_for! {
+    // Unfortunately, nested macros are not allowed, so we list all resource
+    // types manually.
+    //
+    // This is copy-pasted from `resources.rs`, and any updates here should also be updated
+    // there.
+    //
+    // see: https://github.com/rust-lang/rust/issues/35853
+    {
+        crate::websocket::types::room::resources::ResourceType;
+
+        Energy => energy => "energy"
+            => Some(crate::websocket::types::room::resources::ResourceType::Power);
+        Power => power => "power"
+            => Some(crate::websocket::types::room::resources::ResourceType::Hydrogen);
+        Hydrogen => hydrogen => "H"
+            => Some(crate::websocket::types::room::resources::ResourceType::Oxygen);
+        Oxygen => oxygen => "O"
+            => Some(crate::websocket::types::room::resources::ResourceType::Utrium);
+        Utrium => utrium => "U"
+            => Some(crate::websocket::types::room::resources::ResourceType::Lemergium);
+        Lemergium => lemergium => "L"
+            => Some(crate::websocket::types::room::resources::ResourceType::Keanium);
+        Keanium => keanium => "K"
+            => Some(crate::websocket::types::room::resources::ResourceType::Zynthium);
+        Zynthium => zynthium => "Z"
+            => Some(crate::websocket::types::room::resources::ResourceType::Catalyst);
+        Catalyst => catalyst => "X"
+            => Some(crate::websocket::types::room::resources::ResourceType::Ghodium);
+        Ghodium => ghodium => "G"
+            => Some(crate::websocket::types::room::resources::ResourceType::Hydroxide);
+        Hydroxide => hydroxide => "OH"
+            => Some(crate::websocket::types::room::resources::ResourceType::ZynthiumKeanite);
+        ZynthiumKeanite => zynthium_keanite => "ZK"
+            => Some(crate::websocket::types::room::resources::ResourceType::UtriumLemergite);
+        UtriumLemergite => utrium_lemergite => "UL"
+            => Some(crate::websocket::types::room::resources::ResourceType::UtriumHydride);
+        UtriumHydride => utrium_hydride => "UH"
+            => Some(crate::websocket::types::room::resources::ResourceType::UtriumOxide);
+        UtriumOxide => utrium_oxide => "UO"
+            => Some(crate::websocket::types::room::resources::ResourceType::KeaniumHydride);
+        KeaniumHydride => keanium_hydride => "KH"
+            => Some(crate::websocket::types::room::resources::ResourceType::KeaniumOxide);
+        KeaniumOxide => keanium_oxide => "KO"
+            => Some(crate::websocket::types::room::resources::ResourceType::LemergiumHydride);
+        LemergiumHydride => lemergium_hydride => "LH"
+            => Some(crate::websocket::types::room::resources::ResourceType::LemergiumOxide);
+        LemergiumOxide => lemergium_oxide => "LO"
+            => Some(crate::websocket::types::room::resources::ResourceType::ZynthiumHydride);
+        ZynthiumHydride => zynthium_hydride => "ZH"
+            => Some(crate::websocket::types::room::resources::ResourceType::ZynthiumOxide);
+        ZynthiumOxide => zynthium_oxide => "ZO"
+            => Some(crate::websocket::types::room::resources::ResourceType::GhodiumHydride);
+        GhodiumHydride => ghodium_hydride => "GH"
+            => Some(crate::websocket::types::room::resources::ResourceType::GhodiumOxide);
+        GhodiumOxide => ghodium_oxide => "GO"
+            => Some(crate::websocket::types::room::resources::ResourceType::UtriumAcid);
+        UtriumAcid => utrium_acid => "UH2O"
+            => Some(crate::websocket::types::room::resources::ResourceType::UtriumAlkalide);
+        UtriumAlkalide => utrium_alkalide => "UHO2"
+            => Some(crate::websocket::types::room::resources::ResourceType::KeaniumAcid);
+        KeaniumAcid => keanium_acid => "KH2O"
+            => Some(crate::websocket::types::room::resources::ResourceType::KeaniumAlkalide);
+        KeaniumAlkalide => keanium_alkalide => "KHO2"
+            => Some(crate::websocket::types::room::resources::ResourceType::LemergiumAcid);
+        LemergiumAcid => lemergium_acid => "LH2O"
+            => Some(crate::websocket::types::room::resources::ResourceType::LemergiumAlkalide);
+        LemergiumAlkalide => lemergium_alkalide => "LHO2"
+            => Some(crate::websocket::types::room::resources::ResourceType::ZynthiumAcid);
+        ZynthiumAcid => zynthium_acid => "ZH2O"
+            => Some(crate::websocket::types::room::resources::ResourceType::ZynthiumAlkalide);
+        ZynthiumAlkalide => zynthium_alkalide => "ZHO2"
+            => Some(crate::websocket::types::room::resources::ResourceType::GhodiumAcid);
+        GhodiumAcid => ghodium_acid => "GH2O"
+            => Some(crate::websocket::types::room::resources::ResourceType::GhodiumAlkalide);
+        GhodiumAlkalide => ghodium_alkalide => "GHO2"
+            => Some(crate::websocket::types::room::resources::ResourceType::CatalyzedUtriumAcid);
+        CatalyzedUtriumAcid => catalyzed_utrium_acid => "XUH2O"
+            => Some(crate::websocket::types::room::resources::ResourceType::CatalyzedUtriumAlkalide);
+        CatalyzedUtriumAlkalide => catalyzed_utrium_alkalide => "XUHO2"
+            => Some(crate::websocket::types::room::resources::ResourceType::CatalyzedKeaniumAcid);
+        CatalyzedKeaniumAcid => catalyzed_keanium_acid => "XKH2O"
+            => Some(crate::websocket::types::room::resources::ResourceType::CatalyzedKeaniumAlkalide);
+        CatalyzedKeaniumAlkalide => catalyzed_keanium_alkalide => "XKHO2"
+            => Some(crate::websocket::types::room::resources::ResourceType::CatalyzedLemergiumAcid);
+        CatalyzedLemergiumAcid => catalyzed_lemergium_acid => "XLH2O"
+            => Some(crate::websocket::types::room::resources::ResourceType::CatalyzedLemergiumAlkalide);
+        CatalyzedLemergiumAlkalide => catalyzed_lemergium_alkalide => "XLHO2"
+            => Some(crate::websocket::types::room::resources::ResourceType::CatalyzedZynthiumAcid);
+        CatalyzedZynthiumAcid => catalyzed_zynthium_acid => "XZH2O"
+            => Some(crate::websocket::types::room::resources::ResourceType::CatalyzedZynthiumAlkalide);
+        CatalyzedZynthiumAlkalide => catalyzed_zynthium_alkalide => "XZHO2"
+            => Some(crate::websocket::types::room::resources::ResourceType::CatalyzedGhodiumAcid);
+        CatalyzedGhodiumAcid => catalyzed_ghodium_acid => "XGH2O"
+            => Some(crate::websocket::types::room::resources::ResourceType::CatalyzedGhodiumAlkalide);
+        CatalyzedGhodiumAlkalide => catalyzed_ghodium_alkalide => "XGHO2"
+            => None;
+    }
+
+    /// Resource iterator for a `Tombstone`.
+    #[derive(Debug)]
+    pub struct TombstoneContents(Tombstone);
+}
+
+impl Tombstone {
+    /// Iterates over this structure's resources.
+    pub fn resources(&self) -> TombstoneContents {
+        TombstoneContents::new(self)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use serde::Deserialize;
